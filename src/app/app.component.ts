3610d988495f690bb4fd12fbf2efc847ced89c55
seal0112 <seal0112@hotmail.com>
2018-03-12 19:42:33 +0800

    Storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { ContactPage } from '../pages/contact/contact';
 import { DishdetailPage } from '../pages/dishdetail/dishdetail';
 import { FavoritesPage } from '../pages/favorites/favorites';
 import { ReservationPage } from '../pages/reservation/reservation';
+import { LoginPage } from '../pages/login/login';
 
 @Component({
   templateUrl: 'app.html'
@@ -55,4 +56,8 @@ export class MyApp {
     modal.present();
   }
 
+  openLogin() {
+    let modal = this.modalCtrl.create(LoginPage);
+    modal.present();
+  }
 }
